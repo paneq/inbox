1cf85d71c02936bf227543c823f0e4414b722baf
Robert Pankowecki <robert.pankowecki@gmail.com>
2012-09-20 00:27:20 +0200

    Remove empty test files

--- test/functional/inbox/email_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Inbox
-  class EmailTest < ActionMailer::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
--- test/unit/helpers/inbox/emails_helper_test.rb
+++
@@ -1,6 +0,0 @@
-require 'test_helper'
-
-module Inbox
-  class EmailsHelperTest < ActionView::TestCase
-  end
-end
