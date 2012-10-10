18bf7dc8ffcc712d919358da36f6a5b99f2db074
Robert Pankowecki <robert.pankowecki@gmail.com>
2012-10-10 15:36:18 +0200

    hash 1.8.7 syntax compat

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Inbox::Engine.routes.draw do
     resources :emails, :only => [:index, :show, :new, :create], :id => /[^\/]+/
   end
 
-  get "examples/rich_html", to: "examples#rich_html"
+  get "examples/rich_html", :to => "examples#rich_html"
   #get "examples/rich_html" # Rails namespace bug
 end
